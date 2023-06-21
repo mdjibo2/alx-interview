9c4c6ebe68f703b67dd1f76f69f32a6b6b2f2742
mdjibo2 <justpourmestest00@gmail.com>
2023-06-21 23:26:50 +0000

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter
+"""
+
+def island_perimeter(grid):
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                # Check top
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                # Check bottom
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                # Check left
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                # Check right
+                if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
